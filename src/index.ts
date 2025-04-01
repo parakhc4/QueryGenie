95489339c349aee4208ead27e359d556050cf084
Parakh Chaudhary <69101765+parakhc4@users.noreply.github.com>
2025-04-01 16:26:14 +0530

    Set up MonsterAPI LLM API call - Failing currently. Added API Key in gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello from Node + TypeScript');
+import MonsterAPI from 'monsterapi';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const apiKey = process.env.MONSTER_API_KEY;
+if (!apiKey) {
+  throw new Error("MONSTER_API_KEY is not defined in the environment variables.");
+}
+const client = new MonsterAPI(apiKey);
+
+const model = "falcon-7b-instruct"; 
+const input = {
+  prompt:"Hello",
+};
+
+client
+  .generate(model, input)
+  .then((response) => {
+    console.log("Generated content:", response);
+  })
+  .catch((error) => {
+    // Handle API errors
+    console.error("Error:", error);
+  });
